057dda1fc347ccea3da57b8019e77dd8bc82d67d
clashroyaleisgood <oscar19971003@gmail.com>
2022-12-28 20:52:32 +0800

    split step a, b; & new overall rotation csv

--- Inference Data/codes/_run_all.py
+++ Inference Data/codes/_run_all.py
@@ -6,12 +6,20 @@ from d_combine_to_video import combine_video
 
 import os
 
-def ab_step(inference_dataset_name, video_names, video_folders, skip):
-    # step: a
+
+def a_step(inference_dataset_name, video_names, skip):
     extract_videos(inference_dataset_name, video_names, skip)
 
-    # step: b
+
+def b_step(inference_dataset_name, video_folders, resume_at=None):
     for select in range(len(video_folders)):
+        if resume_at is not None:
+            if video_folders[select] != resume_at:
+                continue
+            else:
+                print(f'resume at: {resume_at}')
+                resume_at = None
+
         image_folder = os.path.join(inference_dataset_name, StageName[1], video_folders[select])
 
         bbox = find_bbox(
@@ -28,6 +36,7 @@ def d_step(inference_dataset_name, video_folders, skip, mode):
     ''' mode = {'vr' ,'v', 'r'}
         'v'ideo, 'r'otation
     '''
+    import pandas as pd
     if 'v' in mode:
         for Select in range(len(video_folders)):
             combine_video(
@@ -38,16 +47,21 @@ def d_step(inference_dataset_name, video_folders, skip, mode):
             )  # infer_HandMotion / 3-mobrecon / [0_stone]
 
     if 'r' in mode:
+        overall_series = []
         for Select in range(len(video_folders)):
-            save_csv(
+            min_max_series = save_csv(
                 inference_dataset_name=inference_dataset_name,
                 method=method,
-                video_folder=video_folders[Select]
+                video_folder=video_folders[Select],
+                return_min_max=True
             )
+            overall_series += [min_max_series]
+        overall_df = pd.concat(overall_series, axis=1).transpose().round(2)
+        overall_df.to_csv(os.path.join(inference_dataset_name, 'overall_rotation.csv'))
 
 
 if __name__ == '__main__':
-    inference_dataset_name = 'infer_ROM_data'
+    inference_dataset_name = 'infer_ROM_data_2'
 
     video_names = os.listdir(os.path.join(inference_dataset_name, StageName[0]))
     video_folders = [file.rsplit('.', 1)[0] for file in video_names]
@@ -55,7 +69,8 @@ if __name__ == '__main__':
 
     # step: a, b
     skip = 5
-    ab_step(inference_dataset_name, video_names, video_folders, skip)
+    # a_step(inference_dataset_name, video_names, skip)
+    # b_step(inference_dataset_name, video_folders, resume_at='30U')
 
     # WAIT step: c
     method = 'mobrecon'
